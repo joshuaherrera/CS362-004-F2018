19e5532e6b82af859218aa429c639d2e7f28a98c
Joshua Herrera <herrjosh@oregonstate.edu>
2018-11-29 21:38:47 -0800

    added input partition portion

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -29,11 +29,70 @@ public class UrlValidatorTest extends TestCase {
    public void testYourFirstPartition()
    {
 	 //You can use this function to implement your First Partition testing	   
-
+	   UrlValidator testURL = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   //all return true
+	   String[] validInputPartitionScheme = {"http://www.google.com", 
+			   "ftp://www.foo.com",
+			   "http://www.bar.com",
+			   "http://www.foobar.com",
+			   "http://www.osu.com"};
+	   boolean res = false;
+	   for(int i = 0; i < validInputPartitionScheme.length; i++)
+	   {
+		   try {res = testURL.isValid(validInputPartitionScheme[i]);}
+		   catch(Error e)
+		   {System.out.println(validInputPartitionScheme[i]+" caused "+e);} 
+	       System.out.println(validInputPartitionScheme[i]+" returned "+res); 
+	   }
+	   //all return false
+	   String[] validInputPartitionNoScheme = {"www.google.com", 
+			   "www.foo.com",
+			   "www.bar.com",
+			   "www.foobar.com",
+			   "www.osu.com"};
+	   res = false;
+	   for(int i = 0; i < validInputPartitionNoScheme.length; i++)
+	   {
+		   try {res = testURL.isValid(validInputPartitionNoScheme[i]);}
+		   catch(Error e)
+		   {System.out.println(validInputPartitionNoScheme[i]+" caused "+e);} 
+	       System.out.println(validInputPartitionNoScheme[i]+" returned "+res); 
+	   }
+	   
+       
    }
    
    public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
+		 //You can use this function to implement your Second Partition testing	
+	   UrlValidator testURL = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   //these all return true
+	   String[] invalidInputPartitionScheme = {"http://www..com", 
+			   "http://www.com",
+			   "http://www.bar.com.com",
+			   "http://ww.foobar.com",
+			   "http://www.osu."};
+	   boolean res = false;
+	   for(int i = 0; i < invalidInputPartitionScheme.length; i++)
+	   {
+		   try {res = testURL.isValid(invalidInputPartitionScheme[i]);}
+		   catch(Error e)
+		   {System.out.println(invalidInputPartitionScheme[i]+" caused "+e);} 
+	       System.out.println(invalidInputPartitionScheme[i]+" returned "+res); 
+	   }
+	   //these all return false
+	   String[] invalidInputPartitionNoScheme = {"www..com", 
+			   "www.com",
+			   "www.bar.com.com",
+			   "ww.foobar.com",
+			   "www.osu."};
+	   res = false;
+	   for(int i = 0; i < invalidInputPartitionNoScheme.length; i++)
+	   {
+		   try {res = testURL.isValid(invalidInputPartitionNoScheme[i]);}
+		   catch(Error e)
+		   {System.out.println(invalidInputPartitionNoScheme[i]+" caused "+e);} 
+	       System.out.println(invalidInputPartitionNoScheme[i]+" returned "+res); 
+	   }
 
    }
    //You need to create more test cases for your Partitions if you need to 
